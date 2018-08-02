79ed8b599f0d5d43321bf774d9775b296e0913b7
ninojairo <nino.jairo@gmail.com>
2018-08-01 22:31:59 -0500

    First Commit

---
+++ src/main/java/com/itfactoria/savingsaccount/Cuenta.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itfactoria.savingsaccount;
+
+/**
+ *
+ * @author jnino
+ */
+public class Cuenta {
+    private String titular;
+    private double cantidad;
+
+    public Cuenta(String titular) {
+        //this.titular = titular;
+        this(titular,0);
+    }
+
+    public Cuenta(String titular, double cantidad) {
+        this.titular = titular;
+        this.cantidad = cantidad;
+        if (this.cantidad < 0) {
+            this.cantidad = 0;
+        }
+        
+        
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    /**
+     * @param cantidad
+     */
+    public void ingresar(double cantidad){
+        if (cantidad > 0){
+            this.cantidad += cantidad;
+        }
+    }
+    
+    /**
+     * @param cantidad
+     */
+    public void retirar(double cantidad){
+        if(this.cantidad - Math.abs(this.cantidad)<0){
+            this.cantidad =0;
+        }
+        else{
+            this.cantidad -=cantidad;
+        }
+    }
+    
+    
+}
---
+++ src/test/java/com/itfactoria/savingsaccount/CuentaTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itfactoria.savingsaccount;
+
+import com.itfactoria.savingsaccount.Cuenta;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jnino
+ */
+public class CuentaTest {
+    
+    public CuentaTest() {
+        }
+    
+    
+    @org.junit.Test
+    public void testCrearCuenta() {
+        System.out.println("crear cuenta");
+        /*double cantidad = 0.0;
+        Cuenta instance = null;
+        instance.retirar(cantidad);*/
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+        
+        Cuenta c = new Cuenta("Chikis", -500);
+        assertEquals(0, c.getCantidad(),0);
+        
+        Cuenta c1 = new Cuenta("Negro", 500);
+        assertEquals(500, c1.getCantidad(),0);
+        
+    }
+    
+    
+    
+}
